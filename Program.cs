d9d37aded8dd8e884b5681a8ffa4994c28a88b1b
Kaaner4x <kaaner.emirhan@gmail.com>
2026-06-08 00:09:18 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MatrisGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
